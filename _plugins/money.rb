bea44697e3678eb48d73eb244e2129306b5603a0
Tobias Kunze <r@rixx.de>
2023-04-05 15:56:09 +0200

    Improve money table display

--- _plugins/money.rb
+++ _plugins/money.rb
@@ -5,6 +5,13 @@ module Jekyll
       readable[0] = readable[0].gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{separator}")
       return "#{readable.join('.')}"
     end
+    def categoryName(value, separator=":")
+      # If the value doesn't contain the separator, return it as is
+      return value unless value.include? separator
+      # split the value on the separator, only return the last part,
+      # but prefix it with as many spaces as there are separators
+      return "─" * value.count(separator) + value.split(separator).last
+    end
   end
 end
 
